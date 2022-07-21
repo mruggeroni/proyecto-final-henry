089a566ec2fe5c6c017dec72568e270cff83c822
Bamio Ezequiel <ezequielbamiok@gmail.com>
2022-07-21 20:26:06 -0300

    added regions and featured

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,9 +1,23 @@
+import { 
+    GET_ALL_PACKAGE,
+    GET_PACKAGE_BY_ID,
+    GET_DESTINATIONS,
+    GET_REGIONS,
+    SEARCH_PACKAGES,
+    GET_ACTIVITIES,
+    GET_TYPES,
+    GET_SEASONS,
+    GET_USER
+} from './../actions/index.js';
+
 const initialState = {
     allPackages: [],
     activities: [],
     destinations: [],
+    regions: [],
     types: [],
     seasons: [],
+    featured: [],
     detailPackages: {},
     user: {}
 }
