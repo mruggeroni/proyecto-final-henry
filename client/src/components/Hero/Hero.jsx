ca0bbbb9316a7a0b8acc3a5486b6a93877dc5f0a
Bamio Ezequiel <ezequielbamiok@gmail.com>
2022-07-22 22:42:54 -0300

    fixed components

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from 'react-redux';
+import React from "react";
 import style from './Hero.module.css';
 import FilterSearch from "./FilterSearch/FilterSearch.jsx";
 
