cbdda7ed40645f7e89b658ce47d060ab0565f5fa
Ahrens Omar Gabriel <ahrensgabriel09@gmail.com>
2022-07-23 22:45:13 -0300

    Pending conflit resolved ('PackageRoutes.js')

--- api/routes/PackagesRoutes.js
+++ api/routes/PackagesRoutes.js
@@ -1,39 +1,16 @@
-<<<<<<< HEAD
-import { Router } from 'express';
-
-import { createPackage, getPackages, getFeaturedPackages } from '../controllers/PackagesController.js';
-import { getPackageData } from '../controllers/database/PackagesControllersData.js';
-
-const router = Router();
-
-router.get('/packages', getPackages);
-router.get('/packages/featured', getFeaturedPackages);
-router.post('/packages', createPackage);
-// RUTAS EXCLUSIVAS PARA CARGAR LOS DATOS A LA DATABASE, SI NECESITA CARGAR LOS DATOS A SU DB LOCAL USE ESTAS RUTAS
-//router.get('/dbpackages', getPackageData)
-//
-
-
-// router.get('/packages', (req, res) => {
-// 	res.status(200).send('<h1>Packages Route</h1>');
-// });
-
-=======
 import { Router } from 'express';
 
 import { createPackage, getPackages, getFeaturedPackages, getTypes } from '../controllers/PackagesController.js';
 
 const router = Router();
 
-router.get('/types', getTypes);
+// RUTAS EXCLUSIVAS PARA CARGAR LOS DATOS A LA DATABASE, SI NECESITA CARGAR LOS DATOS A SU DB LOCAL USE ESTAS RUTAS
 router.get('/packages', getPackages);
 router.get('/packages/featured', getFeaturedPackages);
 router.post('/packages', createPackage);
 
+//RUTAS EXCLUSIVAS PARA EL FRONT
 
-// router.get('/packages', (req, res) => {
-// 	res.status(200).send('<h1>Packages Route</h1>');
-// });
+router.get('/types', getTypes);
 
->>>>>>> Gaby
 export default router;
