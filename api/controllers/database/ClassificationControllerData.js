bb70ff6fa1b52b0fe2e26c5174de8b136dc808a0
sadnena <sadodreman@gmail.com>
2022-07-24 21:32:36 -0300

    rutas get extendidas

--- api/controllers/database/ClassificationControllerData.js
+++ api/controllers/database/ClassificationControllerData.js
@@ -1,20 +1,23 @@
 import { Classification } from '../../models/Classification.js';
 import * as fs from 'fs';
-
+import * as data from '../../data/classification.json' assert {type: "json"};
 export const getClassificationData = async () => {
     try {
        //CAMBIAR A PATH RELATIVO
-        let dataJson = fs.readFile('/home/sadnena/pf/proyecto-final-henry/api/data/classification.json', "utf8", (error, data) => {
-        let dataClassification = JSON.parse(data)
+        // let dataJson = fs.readFile('/home/sadnena/pf/proyecto-final-henry/api/data/classification.json', "utf8", (error, data) => {
+        
+        // })
+        let dataClassification = data.default
         dataClassification.map((categoria) => {
             Classification.findOrCreate({
                 where: {name: categoria.name, image: categoria.image }
                 
             })
         })
-        })
-        //let dataClassificationResult = await Classification.findAll()
-        
+        let dataClassificationResult = await Classification.findAll()
+        console.log('POTATO')
+        console.log(dataClassificationResult)
+
          
     }catch (error){
         console.log(error.message)
