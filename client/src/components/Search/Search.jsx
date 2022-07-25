cc6e05735f145b5441e8d0b240d406dfddca6e92
Juan Cruz <juancruzmusso@gmail.com>
2022-07-25 19:44:20 -0300

    arreglado search

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -5,7 +5,14 @@ import { Link } from "react-router-dom";
 import Card from "./Card/Card.jsx";
 import View from "./View.jsx";
 import SortPrice from "./SortPrice.jsx";
-import { filterPackagesByDestination } from "./../../redux/actions/index";
+import {
+  filterPackagesByDestination,
+  getAllActivities,
+  getAllDestinations,
+  getAllPackage,
+  getDestinationsWithPackages,
+  getOnSale,
+} from "./../../redux/actions/index";
 import Paginado from "../Paginado/paginado";
 import s from "./Search.module.css";
 import style from "./Select.module.css";
@@ -13,6 +20,8 @@ import style from "./Select.module.css";
 export default function FilteredSearch() {
   /* 
     cuando estas parado en /search y refrescas, no se carga el estado de allPackages
+    YA ESTA SOLICIONADO.. LOS DESTINOS QUE SE DEBEN CARGAR EN EL FILTRO TAMBIEN... 
+    se soliciono con el useEffect
   */
 
   const dispatch = useDispatch();
@@ -31,7 +40,6 @@ export default function FilteredSearch() {
     indexOfFirstPackage,
     indexOfLastPackages
   );
-
   const paginado = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
@@ -39,64 +47,86 @@ export default function FilteredSearch() {
   const handleChange = (e) => {
     e.preventDefault();
     dispatch(filterPackagesByDestination(e.target.value));
+    dispatch(getAllPackage());
   };
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(async () => {
+    setLoading(true);
+    await dispatch(getAllPackage());
+    await dispatch(getAllDestinations());
+    await dispatch(getOnSale());
+    await dispatch(getAllActivities());
+    await dispatch(getDestinationsWithPackages());
+
+    setLoading(false);
+  }, []);
 
   return (
     <div className={s.container}>
-      <div className={s.view}>
-        <SortPrice setOrder={setOrder} setCurrentPage={setCurrentPage} />
-        <div>
-          <label>Search Package from: </label>
-          <select
-            id="searchDestinations"
-            className={style.select}
-            onChange={(e) => handleChange(e)}
-          >
-            {" "}
-            <option selected={true} disabled="disabled">
-              Seleccionar un Destino
-            </option>
-            <option value="all">Todos los destinos</option>
-            {allDestinations?.map((el) => (
-              <option key={el} value={el}>
-                {el}
-              </option>
-            ))}
-          </select>
+      {loading ? (
+        <div className={s.contenedorSpinner}>
+          <div className={s.spinner}></div>
         </div>
-        <View
-          currentPackage={currentPackage}
-          allPackages={allPackages}
-          indexOfFirstPackage={indexOfFirstPackage}
-          setPackagesPerPage={setPackagesPerPage}
-          setCurrentPage={setCurrentPage}
-          currentPage={currentPage}
-        />
-      </div>
-      <div className={s.cards}>
-        {currentPackage &&
-          currentPackage?.map((p) => {
-            return (
-              <div className={s.eachcard} key={p.id}>
-                <Link to={"/detail/" + p.id} key={p.id}>
-                  <Card
-                    name={p.name}
-                    image={p.main_image}
-                    description={p.description}
-                    price={p.price}
-                    key={p.id}
-                  />
-                </Link>
-              </div>
-            );
-          })}
-      </div>
-      <Paginado
-        packagesPerPage={packagesPerPage}
-        allPackages={allPackages.length}
-        paginado={paginado}
-        currentPage={currentPage}
-      />
+      ) : (
+        <div>
+          <div className={s.view}>
+            <SortPrice setOrder={setOrder} setCurrentPage={setCurrentPage} />
+            <div>
+              <label>Search Package from: </label>
+              <select
+                id="searchDestinations"
+                className={style.select}
+                onChange={(e) => handleChange(e)}
+              >
+                {" "}
+                <option selected={true} disabled="disabled">
+                  Seleccionar un Destino
+                </option>
+                <option value="all">Todos los destinos</option>
+                {allDestinations?.map((el) => (
+                  <option key={el} value={el}>
+                    {el}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <View
+              currentPackage={currentPackage}
+              allPackages={allPackages}
+              indexOfFirstPackage={indexOfFirstPackage}
+              setPackagesPerPage={setPackagesPerPage}
+              setCurrentPage={setCurrentPage}
+              currentPage={currentPage}
+            />
+          </div>
+          <div className={s.cards}>
+            {currentPackage &&
+              currentPackage?.map((p) => {
+                return (
+                  <div className={s.eachcard} key={p.id}>
+                    <Link to={"/detail/" + p.id} key={p.id}>
+                      <Card
+                        name={p.name}
+                        image={p.main_image}
+                        description={p.description}
+                        price={p.price}
+                        key={p.id}
+                      />
+                    </Link>
+                  </div>
+                );
+              })}
+          </div>
+          <Paginado
+            packagesPerPage={packagesPerPage}
+            allPackages={allPackages.length}
+            paginado={paginado}
+            currentPage={currentPage}
+          />
+        </div>
+      )}
     </div>
   );
 }
