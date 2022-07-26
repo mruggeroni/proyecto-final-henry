f8ccc73f176b79fe03b412e87a07e16c1c5a6735
Juan Cruz <juancruzmusso@gmail.com>
2022-07-26 18:36:22 -0300

    post packages arreglado por sadyira

--- api/models/Packages.js
+++ api/models/Packages.js
@@ -1,92 +1,95 @@
 // <<<<<<< HEAD
 //RUTA EXCLUSIVA PARA CARGAR DATOS A LA DATABASE
 
-import { DataTypes } from 'sequelize';
-import { sequelize } from '../db.js';
-import { Destination } from './Destinations.js';
-import { Activity } from './Activities.js';
-export const Package = sequelize.define('package', {
-	name: {
-		type: DataTypes.STRING,
-		allowNull: false,
-		unique: true,
-	},
-	description: {
-		type: DataTypes.TEXT,
-		allowNull: false,
-	},
-	main_image: {
-		type: DataTypes.STRING,
-		defaultValue: 'https://demos.maperez.es/default-main-image.jpg',
-	},
-	images: {
-		type: DataTypes.ARRAY(DataTypes.STRING),
-		allowNull: false,
-	},
-	price: {
-		type: DataTypes.INTEGER,
-		allowNull: false,
-	},
+import { DataTypes } from "sequelize";
+import { sequelize } from "../db.js";
+import { Destination } from "./Destinations.js";
+import { Activity } from "./Activities.js";
+export const Package = sequelize.define(
+  "package",
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    main_image: {
+      type: DataTypes.STRING,
+    },
+    images: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
     start_date: {
-		type: DataTypes.DATEONLY,
-		allowNull: false,
-	},
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
     end_date: {
-		type: DataTypes.DATEONLY,
-		allowNull: false,
-	},
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
     region: {
-		type: DataTypes.ENUM(
-			"Europa Occidental",
-			"Europa Central",
-			"Europa Oriental",
-			"Asia Oriental",
-			"Asia del Sur",
-			"Asia Sudoriental Continental",
-			"Norte América",
-			"Sudamérica",
-			"América Central"
-		),
-		allowNull: false,
-	},
+      type: DataTypes.ENUM(
+        "Europa Occidental",
+        "Europa Central",
+        "Europa Oriental",
+        "Asia Oriental",
+        "Asia del Sur",
+        "Asia Sudoriental Continental",
+        "Norte América",
+        "Sudamérica",
+        "América Central"
+      ),
+      allowNull: false,
+    },
     seasson: {
-		type: DataTypes.ENUM(
-			"Verano",
-			"Otoño",
-			"Invierno",
-			"Primavera",
-			"Especial"
-		),
-		allowNull: false,
-	},
+      type: DataTypes.ENUM(
+        "Verano",
+        "Otoño",
+        "Invierno",
+        "Primavera",
+        "Especial"
+      ),
+      allowNull: false,
+    },
     type: {
-		type: DataTypes.ENUM(
-			"Crucero",
-			"Pack Short",
-			"Pack Large",
-			"Multidestino"
-		),
-		allowNull: false,
-	},
-	featured: {
-		type: DataTypes.BOOLEAN,
-		defaultValue: false,
-	},
-	available: {
-		type: DataTypes.BOOLEAN,
-		defaultValue: true,
-	},
-	on_sale: {
-		type: DataTypes.INTEGER,
-		defaultValue: 0,
-	},
-}, {
-	timestamps: false
-});
-Package.belongsToMany(Destination, {through: 'Package_Destination'})
-Destination.belongsToMany(Package, {through: 'Package_Destination'})
-Package.belongsToMany(Activity, {through: 'Package_Activity'})
-Activity.belongsToMany(Package, {through: 'Package_Activity'})
+      type: DataTypes.ENUM(
+        "Crucero",
+        "Pack Short",
+        "Pack Large",
+        "Multidestino"
+      ),
+      allowNull: false,
+    },
+    featured: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    available: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+    on_sale: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+Package.belongsToMany(Destination, { through: "Package_Destination" });
+Destination.belongsToMany(Package, { through: "Package_Destination" });
+Package.belongsToMany(Activity, { through: "Package_Activity" });
+Activity.belongsToMany(Package, { through: "Package_Activity" });
 // =======
 // //RUTA EXCLUSIVA PARA CARGAR DATOS A LA DATABASE
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import store from './redux/store/index.js';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import store from "./redux/store/index.js";
+import { Provider } from "react-redux";
 import { Auth0Provider } from "@auth0/auth0-react";
-import claves from "./privado"
+import claves from "./privado";
 
 ReactDOM.render(
   <Provider store={store}>
@@ -14,12 +14,12 @@ ReactDOM.render(
       clientId={claves.CLIENTID}
       // redirectUri={window.location.origin}
       redirectUri={"http://localhost:3000"}
-      audiencie="identificador unico" //deberia de ser privado -> lo usamos en las rutas tmb
-      audience="identificador unico"
+      audiencie={claves.AUDIENCIE} //deberia de ser privado -> lo usamos en las rutas tm
+      audience={claves.AUDIENCIE}
       scope="openid profile email"
     >
       <App />
     </Auth0Provider>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
