cde8ab94546ffaf353f45d732b7cc48aabf95cd5
sadnena <sadodreman@gmail.com>
2022-07-27 14:44:29 -0300

    cambios

--- api/models/Packages.js
+++ api/models/Packages.js
@@ -67,51 +67,6 @@ export const Package = sequelize.define('package', {
  {
 	timestamps: false
 });
-export const User = sequelize.define('user', {
-	first_name: {
-		type: DataTypes.STRING,
-		allowNull: false,
-	},
-	last_name: {
-		type: DataTypes.STRING,
-		allowNull: false,
-	},
-	email: {
-		type: DataTypes.STRING,
-        allowNull: false,
-		unique: true,
-	},
-	// country: {
-	// 	type: DataTypes.STRING,
-	// 	allowNull: true,
-	// },
-	// city: {
-	// 	type: DataTypes.STRING,
-	// 	allowNull: true,
-	// },
-    adress: {
-		type: DataTypes.TEXT,
-		allowNull: true,
-	},
-    phone: {
-		type: DataTypes.STRING,
-		allowNull: true,
-	},
-
-	is_admin: {
-		type: DataTypes.BOOLEAN,
-        defaultValue: false,
-        allowNull: false,
-
-	},
-	photo: {
-		type: DataTypes.STRING,
-        allowNull: true,
-	},
-}, 
-{
-	timestamps: false
-});
 Package.belongsToMany(Destination, {through: 'Package_Destination'})
 Destination.belongsToMany(Package, {through: 'Package_Destination'})
 Package.belongsToMany(Activity, {through: 'Package_Activity'})
