95f48258042f914675a4430ad470e85169f0d96b
Bamio Ezequiel <ezequielbamiok@gmail.com>
2022-07-28 23:00:18 -0300

    oik

--- client/src/components/Navbar/PopUps/UserPopOut.jsx
+++ client/src/components/Navbar/PopUps/UserPopOut.jsx
@@ -7,15 +7,17 @@ import { NavLink } from "react-router-dom";
 
 export default function UserPopOut() {
   const { user, isAuthenticated, loginWithPopup, logout } = useAuth0();
-
-    
+  const [showUser, setShowUser] = useState(false);
+  const handleShowUser = () => { setShowUser(true) }
+  const handleNotShowUser = () => { setShowUser(false) }
 
   function handleClick(e) {
     e.preventDefault();
+    setShowUser(!showUser)
     // if (e.target.id === "menuProfile") {
-    document
-      .getElementById("profile_container")
-      .classList.toggle(`${style.open_profile}`);
+    // document
+      // .getElementById("profile_container")
+      // .classList.toggle(`${style.open_profile}`);
 
   }
 
@@ -27,7 +29,7 @@ export default function UserPopOut() {
     <div className={style.profile_container}>
       <button
         id="menuProfile"
-        onClick={(e) => handleClick(e)}
+        onClick={handleClick}
         className={style.userIcon}
       >
         <img
@@ -37,10 +39,10 @@ export default function UserPopOut() {
           className={style.nav_item_profile}
         />
       </button>
-      <div id="profile_container" className={style.user_profile_container}>
-        {/* <div onClick={handleClick} className={style.back}>
-
-        </div> */}
+      {
+        console.log(showUser)
+      }
+      <div id="profile_container" className={`${style.user_profile_container} ${showUser ? style.open_profile : null}`}>
         <img src={user.photo} alt={user.name} />
         <p>{user.name}</p>
         <hr className={style.create_line} />
