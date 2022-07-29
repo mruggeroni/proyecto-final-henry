86e67aa70ca13ee385e2efd4de642a82ec9252d8
maperezotero <maperezotero@gmail.com>
2022-07-29 17:23:17 +0200

    Added create order

--- api/controllers/OrdersController.js
+++ api/controllers/OrdersController.js
@@ -1,7 +1,7 @@
 import { User } from "../models/Users.js";
 import { Order } from "../models/Orders.js";
-import { OrderItem } from "../models/OrderItems.js";
-import { Package } from '../models/Packages.js';
+// import { OrderItem } from "../models/OrderItems.js";
+import { Package, OrderItem } from '../models/Packages.js';
 
 export const getOrders = async (req, res) => {
 	try {
@@ -44,14 +44,21 @@ export const getOrderDetail = async (req, res) => {
 }
 
 export const createOrder = async (req, res) => {
-	const { userId, totalOrder } = req.body;
+	const { userId, total_order, quantity, packageId } = req.body;
 
 	try {
 		const newOrder = await Order.create({
 			userId,
-			totalOrder,
+			total_order,
 		});
+		
+		const paquete = await Package.findByPk(packageId);
+		console.log(paquete.id, paquete.name);
+
+		await newOrder.addPackage(paquete, {through: OrderItem})
+		
+		res.status(200).json(newOrder);
 	} catch (error) {
 		return res.status(500).json({ error: error.message });
 	}
-}
+}	
