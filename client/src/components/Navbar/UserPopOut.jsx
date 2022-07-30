bb83d039fe1710ea39f93dd245af7cada423de4a
Ahrens Omar Gabriel <ahrensgabriel09@gmail.com>
2022-07-29 22:09:00 -0300

    Probando middleware

--- client/src/components/Navbar/UserPopOut.jsx
+++ client/src/components/Navbar/UserPopOut.jsx
@@ -17,12 +17,6 @@ export default function UserPopOut() {
       document
         .getElementById("profile_container")
         .classList.toggle(`${style.open_profile}`);
-      const token = await getAccessTokenSilently()
-      await axios.post("/prueba",{
-        headers:{
-          authorization: `Bearer ${token}`
-        }}
-      );
     } catch (error) {
       console.log(error.message)
     }
