7e3f6d848cc5bf5c144bafc572ae037e9b22e1f5
Juan Cruz <juancruzmusso@gmail.com>
2022-08-03 01:10:41 -0300

    uep

--- client/src/components/UserEdit/Menus/MyProfile/MyProfile.jsx
+++ client/src/components/UserEdit/Menus/MyProfile/MyProfile.jsx
@@ -58,28 +58,28 @@ export default function MyProfile({ showProfile, setShowProfile }) {
         e.preventDefault();
         // if(input.first_name !== user.first_name || input.last_name !== user.last_name){
 
-            Swal.fire({
-                title: 'Estas seguro que desea modificar sus datos?',
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Guardar'
-            }).then(async (result) => {
-                if (result.isConfirmed) {
-                    let resUpdated;
-                    if (archivo[0]) {
-                        const files = e.target.files;
-                        const data = new FormData();
-                        data.append("file", archivo[0]);
-                        data.append("upload_preset", "emhwd5ue");
-                        const res = await axios.post(
-                            "https://api.cloudinary.com/v1_1/duie0xk67/image/upload",
-                            data);
-                        resUpdated = await dispatch(updateUser(user.id, { ...input, photo: res.data.secure_url }));
-                    } else {
-                        resUpdated = await dispatch(updateUser(user.id, input));
-                    }
+        Swal.fire({
+            title: 'Estas seguro que desea modificar sus datos?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Guardar'
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                let resUpdated;
+                if (archivo[0]) {
+                    const files = e.target.files;
+                    const data = new FormData();
+                    data.append("file", archivo[0]);
+                    data.append("upload_preset", "emhwd5ue");
+                    const res = await axios.post(
+                        "https://api.cloudinary.com/v1_1/duie0xk67/image/upload",
+                        data);
+                    resUpdated = await dispatch(updateUser(user.id, { ...input, photo: res.data.secure_url }));
+                } else {
+                    resUpdated = await dispatch(updateUser(user.id, input));
+                }
                 Swal.fire(
                     'Datos actualizados!',
                     'Se actualizaron tus datos correctamente!',
@@ -97,7 +97,7 @@ export default function MyProfile({ showProfile, setShowProfile }) {
                 }, 0);
                 setShowProfile(false)
             }
-            })
+        })
         // } else {
         //     Swal.fire({
         //         title: 'Ingresa datos diferentes',
@@ -196,86 +196,12 @@ export default function MyProfile({ showProfile, setShowProfile }) {
                             }
                         </div>
 
-                        <button onClick={handleSubmit} className={s.profile_btn_save}>Guardar cambios</button>
+                        <button onClick={handleSubmit} disabled={isChange || Object.keys(errors).length > 0} className={s.profile_btn_save}>Guardar cambios</button>
                     </div>
                 </form>
             </div>
     );
-//         </div>
-//         <div className={s.profile_input_container}>
-//             <label className={s.profile_label}>Nombre</label>
-//             <input type='text' 
-//                     name='first_name' 
-//                     value={input.first_name} 
-//                     onChange={handleChange} 
-//                     className={s.profile_input} />
-//                     <div></div>
-//         {
-//             errors.first_name && <h4 className={s.profile_error}>{errors.first_name}</h4>
-//         }
-//         </div>
-//         <div className={s.profile_input_container}>
-//             <label className={s.profile_label}>Apellido</label>
-//             <input type='text' 
-//                     name='last_name' 
-//                     value={input.last_name} 
-//                     onChange={handleChange} 
-//                     className={s.profile_input} />
-//         {
-//             errors.last_name && <h4 className={s.profile_error}>{errors.last_name}</h4>
-//         }
-//         </div>
-//         <div className={s.profile_input_container}>
-//             <label className={s.profile_label}>Numero de telefono</label>
-//             <input type='text' 
-//                     name='phone' 
-//                     value={input.phone} 
-//                     onChange={handleChange} 
-//                     className={s.profile_input} />
-//         {
-//             errors.phone && <h4 className={s.profile_error}>{errors.phone}</h4>
-//         }
-//         </div>
-//         <div className={s.profile_input_container}>
-//             <label className={s.profile_label}>Ciudad</label>
-//             <input type='text' 
-//                     name='city' 
-//                     value={input.city} 
-//                     onChange={handleChange} 
-//                     className={s.profile_input} />
-//         {
-//             errors.city && <h4 className={s.profile_error}>{errors.city}</h4>
-//         }
-//         </div>
-//         <div className={s.profile_input_container}>
-//             <label className={s.profile_label}>Estado</label>
-//             <input type='text' 
-//                     name='state' 
-//                     value={input.state} 
-//                     onChange={handleChange} 
-//                     className={s.profile_input} />
-//         {
-//             errors.state && <h4 className={s.profile_error}>{errors.state}</h4>
-//         }
-//         </div>
-//         <div className={s.profile_input_container}>
-//             <label className={s.profile_label}>Codigo postal</label>
-//             <input type='text' 
-//                     name='postal_code' 
-//                     value={input.postal_code} 
-//                     onChange={handleChange} 
-//                     className={s.profile_input} />
-//         {
-//             errors.postal_code && <h4 className={s.profile_error}>{errors.postal_code}</h4>
-//         }
-//         </div>
-        
-//         <button onClick={handleSubmit} disabled={isChange || Object.keys(errors).length > 0} className={s.profile_btn_save}>Guardar cambios</button>
-//         </div>
-//         </form>
-//     </div>
-//   );
-// }
+}
 
 /* 
 const handleClickImage = async (e) => {
@@ -297,4 +223,5 @@ const handleClickImage = async (e) => {
             alert('Debes seleccionar una imagen');
         }
         alert('Debes seleccionar una imagen');
-        } */
+        }
+*/
