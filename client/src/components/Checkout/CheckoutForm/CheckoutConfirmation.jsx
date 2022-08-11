14d044ac5c3819509612514acb12ecfab35167cb
Bamio Ezequiel <ezequielbamiok@gmail.com>
2022-08-11 03:06:12 -0300

    ok

--- client/src/components/Checkout/CheckoutForm/CheckoutConfirmation.jsx
+++ client/src/components/Checkout/CheckoutForm/CheckoutConfirmation.jsx
@@ -30,7 +30,11 @@ export default function CheckoutConfirmation() {
           <BsFillPatchCheckFill className={s.confirmation_icon} />
         </span>
         <h1>Su compra fue realizada con éxito!</h1>
-        <p>Recibirá un mail a {user.email || 'su casilla de mensajes '} con los datos de su compra.</p>
+        <p>
+          Recibirá un mail a {user.email || 'su casilla de mensajes '} con los datos de su compra.
+          <br />
+          En la brevedad nos contactaremos para coordinar los detalles de los pasajeros.
+        </p>
         <button onClick={ () => navigate('/') } className={s.confirmation_btn}>Inicio</button>
       </div>
     </div>
